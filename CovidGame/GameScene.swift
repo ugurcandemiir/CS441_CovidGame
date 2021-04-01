8645f4d4c195170c82db653eb77c8d5c72fd7c67
ugurcandemiir <ugurcandemiir@gmail.com>
2021-03-31 23:04:17 -0400

    Tried to implement Launched Screen

--- CovidGame/GameScene.swift
+++ CovidGame/GameScene.swift
@@ -26,7 +26,14 @@ class GameScene: SKScene {
     let waves = Bundle.main.decode([Wave].self, from: "waves.json")
     let enemyTypes = Bundle.main.decode([EnemyType].self, from: "enemy-types.json")
     
+    var isPlayerAlive = true
+    var levelNumber = 0
+    var waveNumber = 0
+
+    let positions = Array(stride(from: -320, through: 320, by: 80))
+    
     override func didMove(to view: SKView) {
+        physicsWorld.gravity = .zero
         if let particles = SKEmitterNode(fileNamed: "Startfield"){
         particles.position = CGPoint(x:1080,y:0)
             particles.advanceSimulationTime(60)
@@ -44,6 +51,55 @@ class GameScene: SKScene {
         player.physicsBody?.collisionBitMask = CollisionType.enemy.rawValue | CollisionType.enemyWeapon.rawValue
         player.physicsBody?.contactTestBitMask = CollisionType.enemy.rawValue | CollisionType.enemyWeapon.rawValue
         player.physicsBody?.isDynamic = false
+
+    }
+    
+    func createWave() {
+        guard isPlayerAlive else { return }
+
+        if waveNumber == waves.count {
+            levelNumber += 1
+            waveNumber = 0
+        }
+
+        let currentWave = waves[waveNumber]
+        waveNumber += 1
+
+        let maximumEnemyType = min(enemyTypes.count, levelNumber + 1)
+        let enemyType = Int.random(in: 0..<maximumEnemyType)
+
+        let enemyOffsetX: CGFloat = 100
+        let enemyStartX = 600
+
+        if currentWave.enemies.isEmpty {
+            for (index, position) in positions.shuffled().enumerated() {
+                let enemy = EnemyNode(type: enemyTypes[enemyType], startPosition: CGPoint(x: enemyStartX, y: position), xOffset: enemyOffsetX * CGFloat(index * 3), moveStraight: true)
+                addChild(enemy)
+            }
+        } else {
+            for enemy in currentWave.enemies {
+                let node = EnemyNode(type: enemyTypes[enemyType], startPosition: CGPoint(x: enemyStartX, y: positions[enemy.position]), xOffset: enemyOffsetX * enemy.xOffset, moveStraight: enemy.moveStraight)
+                addChild(node)
+            }
+        }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+
+        for child in children {
+            if child.frame.maxX < 0 {
+                if !frame.intersects(child.frame) {
+                    child.removeFromParent()
+                }
+            }
+        }
+
+        let activeEnemies = children.compactMap { $0 as? EnemyNode }
+
+        if activeEnemies.isEmpty {
+            createWave()
+        }
+
 
     }
 

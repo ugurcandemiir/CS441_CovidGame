76129d320e3edd55d5c75b5fc4db60ec290b479f
ugurcandemiir <ugurcandemiir@gmail.com>
2021-04-03 13:41:35 -0400

    Fixed the fire functionality

--- CovidGame/GameScene.swift
+++ CovidGame/GameScene.swift
@@ -7,6 +7,8 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
+
 
 enum CollisionType:UInt32{
     
@@ -17,10 +19,11 @@ enum CollisionType:UInt32{
 }
 
 
-class GameScene: SKScene {
+class GameScene: SKScene,SKPhysicsContactDelegate {
     
 //    private var label : SKLabelNode?
 //    private var spinnyNode : SKShapeNode?
+    let motionManager = CMMotionManager()
     let player = SKSpriteNode(imageNamed: "player")
     
     let waves = Bundle.main.decode([Wave].self, from: "waves.json")
@@ -29,12 +32,15 @@ class GameScene: SKScene {
     var isPlayerAlive = true
     var levelNumber = 0
     var waveNumber = 0
-    
+    var playerShields = 10
+
 
     let positions = Array(stride(from: -320, through: 320, by: 80))
     
     override func didMove(to view: SKView) {
         physicsWorld.gravity = .zero
+        physicsWorld.contactDelegate = self
+
         if let particles = SKEmitterNode(fileNamed: "Startfield"){
             particles.position = CGPoint(x:1080,y:0)
             particles.advanceSimulationTime(60)
@@ -52,6 +58,9 @@ class GameScene: SKScene {
         player.physicsBody?.collisionBitMask = CollisionType.enemy.rawValue | CollisionType.enemyWeapon.rawValue
         player.physicsBody?.contactTestBitMask = CollisionType.enemy.rawValue | CollisionType.enemyWeapon.rawValue
         player.physicsBody?.isDynamic = false
+        
+        motionManager.startAccelerometerUpdates()
+
 
     }
     
@@ -114,6 +123,75 @@ class GameScene: SKScene {
         }
 
 
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard isPlayerAlive else { return }
+
+        let shot = SKSpriteNode(imageNamed: "playerWeapon")
+        shot.name = "playerWeapon"
+        shot.position = player.position
+
+        shot.physicsBody = SKPhysicsBody(rectangleOf: shot.size)
+        shot.physicsBody?.categoryBitMask = CollisionType.playerWeapon.rawValue
+        shot.physicsBody?.collisionBitMask = CollisionType.enemy.rawValue | CollisionType.enemyWeapon.rawValue
+        shot.physicsBody?.contactTestBitMask = CollisionType.enemy.rawValue | CollisionType.enemyWeapon.rawValue
+        addChild(shot)
+
+        let movement = SKAction.move(to: CGPoint(x: 1900, y: shot.position.y), duration: 5)
+        let sequence = SKAction.sequence([movement, .removeFromParent()])
+        shot.run(sequence)
+    }
+    func didBegin(_ contact: SKPhysicsContact) {
+        guard let nodeA = contact.bodyA.node else { return }
+        guard let nodeB = contact.bodyB.node else { return }
+
+        let sortedNodes = [nodeA, nodeB].sorted { $0.name ?? "" < $1.name ?? "" }
+        let firstNode = sortedNodes[0]
+        let secondNode = sortedNodes[1]
+
+        if secondNode.name == "player" {
+            guard isPlayerAlive else { return }
+
+            if let explosion = SKEmitterNode(fileNamed: "Explosion") {
+                explosion.position = firstNode.position
+                addChild(explosion)
+            }
+
+            playerShields -= 1
+
+            if playerShields == 0 {
+                gameOver()
+                secondNode.removeFromParent()
+            }
+
+            firstNode.removeFromParent()
+        } else if let enemy = firstNode as? EnemyNode {
+            enemy.shields -= 1
+
+            if enemy.shields == 0 {
+                if let explosion = SKEmitterNode(fileNamed: "Explosion") {
+                    explosion.position = enemy.position
+                    addChild(explosion)
+                }
+
+                enemy.removeFromParent()
+            }
+
+            if let explosion = SKEmitterNode(fileNamed: "Explosion") {
+                explosion.position = enemy.position
+                addChild(explosion)
+            }
+
+            secondNode.removeFromParent()
+        } else {
+            if let explosion = SKEmitterNode(fileNamed: "Explosion") {
+                explosion.position = secondNode.position
+                addChild(explosion)
+            }
+
+            firstNode.removeFromParent()
+            secondNode.removeFromParent()
+        }
     }
 
 }
